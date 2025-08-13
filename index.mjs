0a83b72c9669326fc186b9b3872e351906b38fdf
Josh Junon <git@josh.junon.me>
2025-08-13 19:18:48 +0200

    fix error case return object

--- index.mjs
+++ index.mjs
@@ -3,7 +3,7 @@ import fsp from "node:fs/promises";
 import { compile } from "@surplus/compiler";
 
 const BASE_RESULT = {
-	pluginName: 'Surplus Compiler'
+	pluginName: "Surplus Compiler",
 };
 
 export default (opts) => ({
@@ -20,21 +20,31 @@ export default (opts) => ({
 						source,
 						minify: false,
 						sourcemapFilename: args.path,
-						...opts
+						...opts,
 					});
+
+					if (result.errors.length > 0) {
+						return {
+							...BASE_RESULT,
+							errors: result.errors?.map((err) => ({
+								text: err,
+							})),
+						};
+					}
 
 					return {
 						...BASE_RESULT,
 						contents: result.code,
-						errors: result.errors?.map(err => ({text: err})),
-						warnings: result.warnings?.map(err => ({text: err})),
+						warnings: result.warnings?.map((err) => ({
+							text: err,
+						})),
 						loader: "default",
 					};
 				} catch (err) {
 					// Handle any errors from the transpiler
 					return {
 						...BASE_RESULT,
-						errors: [{ text: err.message }]
+						errors: [{ text: err.message }],
 					};
 				}
 			});
